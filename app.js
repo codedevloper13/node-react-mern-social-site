52eda8717bcecd747265a79d047e1b1cccc0dead
Abhijit Biswas <abhijitbiswas@live.com>
2021-07-08 20:29:19 +0530

    Mongo db Connect

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 // dor env initialize.
 dotenv.config();
+
+// Mongoose connect
+
+mongoose
+  .connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+  })
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log(err));
+
 // required Routes File
 const postRoutes = require('./routes/post');
 
