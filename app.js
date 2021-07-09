420b0812953afbe0cbec5fbfe4126605e6ddfa57
Abhijit Biswas <abhijitbiswas@live.com>
2021-07-09 21:05:36 +0530

    insert data into mongo db

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 // dor env initialize.
 dotenv.config();
@@ -9,11 +10,11 @@ dotenv.config();
 // Mongoose connect
 
 mongoose
-  .connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => console.log('DB Connected'))
   .catch((err) => console.log(err));
@@ -23,6 +24,7 @@ const postRoutes = require('./routes/post');
 
 /* Middleware Setup */
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 // Routes
 app.use('/', postRoutes);
 
--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,4 @@
+const Post = require('../models/post');
 exports.getPosts = (req, res) => {
   res.json({
     posts: [
@@ -8,5 +9,19 @@ exports.getPosts = (req, res) => {
         title: 'second JS',
       },
     ],
+  });
+};
+
+exports.createPosts = (req, res) => {
+  const post = new Post(req.body);
+  post.save((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.status(200).json({
+      post: result,
+    });
   });
 };
